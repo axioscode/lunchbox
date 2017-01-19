fc60b0bdf0677f6f29403304764e3d889a6a05dd
Alex Duner <asduner@gmail.com>
2017-01-19 16:11:04 -0500

    Enforce design language

--- www/js/quotable.js
+++ www/js/quotable.js
@@ -60,11 +60,13 @@ function processText() {
 }
 
 function saveImage() {
-    // // first check if the quote actually fits
-    // if (($source.offset().top + $source.height()) > $logoWrapper.offset().top) {
-    //     alert("Your quote doesn't quite fit. Shorten the text or choose a smaller font-size.");
-    //     return;
-    // }
+    // first check if the quote actually fits
+    // console.log("QUOTE TOP", $quote.offset().top);
+    // console.log("LOGO BOTTOM", $(window).height() - $logoWrapper.offset().top - $logoWrapper.height());
+    if (($quote.offset().top) > $text.offset().top + 50) {
+        alert("Your quote doesn't quite fit. Shorten the text or choose a smaller font-size.");
+        return;
+    }
 
     // don't print placeholder text if source is empty
     if ($source.text() === '') {
